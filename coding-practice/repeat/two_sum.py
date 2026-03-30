e7195ebe21e0082de6d763883e7f5c23fd299e30
andiewy <and.w736@gmail.com>
2026-03-30 22:41:16 +1100

    valid_ana

--- coding-practice/repeat/two_sum.py
+++ coding-practice/repeat/two_sum.py
@@ -10,11 +10,16 @@ def two_sum(nums, target):
             return[store[match],i] #store[match] --> gets me the array index
 
         store[num]=i
-
+    
+    return []
     
 print(two_sum([2,1,0,3],5)) #[0,3]
 print(two_sum([2,1,-2,3],0)) #[0,2]
 print(two_sum([2,2,-2,3],0)) #[0,2]
 print(two_sum([1,2,3,3],5)) #[1,2]
+#needs " if not isinstance(num,(int,float)): "
 print(two_sum(["a",2,3,3],5)) #[1,2]
+#boolean ture behaves like 1
 print(two_sum([True,2,3,3],5)) #[1,2]
+#no solution
+print(two_sum([2,1,-2,3],6)) #[] --> return [] delete if sol = None
---
+++ coding-practice/valid_anagram.py
@@ -0,0 +1,38 @@
+def sorted_valid_anagram(s,t):
+    if sorted(s) == sorted(t):
+        return True
+    return False
+
+
+
+def valid_anagram(s,t):
+    if len(s) != len(t):
+        return False
+    
+    word = {}
+    
+    #what letters exist in s and how many of each
+    for char in s:
+        word[char]=word.get(char,0)+1
+        
+    #compare with whats in t with s
+    for char in t:
+        if char not in word or word[char] == 0:
+            return False
+        word[char]-=1 #check if letter count matchs work backwards
+            
+    return True
+    
+print(valid_anagram("car", "rac")) #True
+print(valid_anagram("great", "regat")) #True
+print(valid_anagram("grin", "irne")) #False
+#more letters
+print(valid_anagram("grin", "irngg")) #False 
+#no letters
+print(valid_anagram("", "")) #True
+
+
+
+#caps
+#non-char
+
