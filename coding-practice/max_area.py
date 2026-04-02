3ede2caba7e2bc3053858d7076f05387b818ccb2
andiewy <and.w736@gmail.com>
2026-04-03 10:58:20 +1100

    save day 4 again

---
+++ coding-practice/max_area.py
@@ -0,0 +1,13 @@
+#planning
+  |
+| |     |
+|_|_|_|_|_|
+
+def max_area(height):
+    
+    
+    
+
+print(max_area([1,8,6,2,5,4,8,3,7]))  # 49
+print(max_area([1,1]))                # 1
+print(max_area([4,3,2,1,4]))          # 16
